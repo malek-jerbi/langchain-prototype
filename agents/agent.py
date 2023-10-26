717b2d8201e563b072f7176fe3fa16b9ae83d114
Malek Jerbi <malek.jerbi@gmail.Com>
2023-10-25 21:28:29 -0400

    use agent with type openai functions instead of reAct

--- agents/agent.py
+++ agents/agent.py
@@ -1,25 +1,10 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.agents import initialize_agent, Tool, AgentType, load_tools
-from tools.tools import google_search
-from sql_agent import sql_agent
+from langchain.agents import initialize_agent, AgentType
+from tools.tools import tools
 
 
 def lookup(prompt: str) -> str:
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    tools = load_tools(["openweathermap-api"], llm)
-    tools.extend([
-        Tool(
-            name="Crawl_Google",
-            func=google_search,
-            description="useful for when you need to search on google",
-        ),
-        Tool(
-            name="personal_info_agent",
-            func=sql_agent,
-            description="useful for when you need to administrative personal data, for example, social insurance number. this is another agent, so the input can be in natural language.",
-        )
-    ]
-    )
 
     agent = initialize_agent(
         tools=tools,
--- tools/tools.py
+++ tools/tools.py
@@ -1,14 +1,41 @@
 from langchain.serpapi import SerpAPIWrapper
-# from langchain.utilities import OpenWeatherMapAPIWrapper
+from pydantic.v1 import BaseModel, Field
+from langchain.agents import tool
+from langchain.utilities.openweathermap import OpenWeatherMapAPIWrapper
+from agents.agent import sql_agent
 
 
-def google_search(text: str) -> str:
-    """Makes a google search"""
-    search = SerpAPIWrapper()
-    res = search.run(f"{text}")
-    return res
+class WeatherInput(BaseModel):
+    location: str = Field(
+        description="Input should be a location string (e.g. London,GB).")
 
-# def get_weather(location: str) -> str:
-#     """Retrieves the weather"""
-#     weather = weather.run(location)
-#     print(weather)
+
+@tool("get_weather_by_location", args_schema=WeatherInput)
+def get_weather(location: str) -> str:
+    """A wrapper around OpenWeatherMap API. Useful for fetching current weather information for a specified location. """
+    return OpenWeatherMapAPIWrapper().run(location)
+
+
+class GoogleSearchInput(BaseModel):
+    input: str = Field(
+        description="Text based input for google search.")
+
+
+@tool("google_search", args_schema=GoogleSearchInput)
+def google_search(input: str) -> str:
+    """A wrapper around SerpAPO. Useful for making google searches. """
+    return SerpAPIWrapper().run(input)
+
+
+class SQL_agentInput(BaseModel):
+    input: str = Field(
+        description="Natural language text based input for SQL agent.")
+
+
+@tool("prompt_sql_agent", args_schema=SQL_agentInput)
+def prompt_sql_agent(input: str) -> str:
+    """Agent that returns personal data. useful for when you need administrative data, for example, social insurance number."""
+    return sql_agent(input)
+
+
+tools = [get_weather, google_search, prompt_sql_agent]
